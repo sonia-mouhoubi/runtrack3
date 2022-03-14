bed1cad4a6cc6d3b453d2834b74f4a3512da0c35
M <sonia.mouhoubi@laplateforme.io>
2022-03-14 15:45:40 +0100

    Afficher fizzbuzz selon le modulo

---
+++ jour01/job06/script.js
@@ -0,0 +1,19 @@
+function fizzbuzz() {
+   for (let i=1; i<=151; i++) { 
+      if (i % 5 == 0 && i % 3 == 0) { 
+         console.log('FizzBuzz');
+      }
+      else if (i % 3 == 0) { //  
+         console.log('Fizz');
+      }
+      else if (i % 5 == 0) { 
+         console.log('Buzz');
+      }
+      // else {
+      //    console.log('FizzBuzz');
+   
+      // }
+   }
+}
+
+fizzbuzz();
