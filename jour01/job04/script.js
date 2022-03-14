54381666e705850ad0181bbdfcbc014dfc36304b
M <sonia.mouhoubi@laplateforme.io>
2022-03-14 14:16:14 +0100

    function pour savoir si l'année est bisextile

---
+++ jour01/job04/script.js
@@ -0,0 +1,7 @@
+// Une année est bissextile elle est divisible par 4 et non divisible par 100 ou bien divisible par 400.
+function bisextile(annee)
+{
+    if ((annee%4==0) && ((annee%100!=0) || (annee%400==0))) return true;
+    else return false;
+}
+console.log(bisextile(2021));
