564fe89c1c6cffd4c81f7239196abdb88f764af3
M <sonia.mouhoubi@laplateforme.io>
2022-03-24 16:03:54 +0100

    RUNTRACK3 JOUR2 JOB4

---
+++ jour02/job04/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+   let textarea = document.querySelector('#keylogger');
+
+   document.addEventListener('keydown',function(event) {
+      let textareaLettre = textarea.value;
+      let lettre = event.key;
+      textarea.value =  textareaLettre + lettre ;
+   });
+});
