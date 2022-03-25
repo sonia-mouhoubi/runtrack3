8469fa771e0991abd5ef7e24a95c0c81ce0162cb
M <sonia.mouhoubi@laplateforme.io>
2022-03-25 10:12:07 +0100

    RUNTRACK3 JOUR3 JOB1

---
+++ jour03/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="jquery-3.6.0.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+    <button id="button">le $ est un bon élément monétaire. Le # de twitter est bonne I D. Il faudra faire le point avec la classe pour cacher cet élément.</button>
+    <button id="hide">Cacher</button>
+</body>
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () { // Condition pour mettre faire charger le js après le html et css
+    $('#button').on("click", function () { // On récupère l'event au click 
+        $('#button').empty(); // On vide le contenu du bouton 
+        $('#button').append('toto'); // On le remplace par un nouveau contenu
+        // $('#button').html('') permet  de faire la même chose que append()
+    });
+
+    $('#hide').on("click", function () { // Au click du 2eme bouton 
+        $('#button').hide(); // On cache le deuxieme
+    });
+});
